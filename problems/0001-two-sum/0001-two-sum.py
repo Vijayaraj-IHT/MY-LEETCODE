1c1e17b41edf6ed7c5347c3dba7eb433906a19ac
Vijayaraj-IHT <kpvijay3102@gmail.com>
2026-03-14 12:00:01 +0000

    LeetSyncMaster - Two Sum (Python)

--- problems/0001-two-sum/0001-two-sum.py
+++ problems/0001-two-sum/0001-two-sum.py
@@ -1,9 +1,12 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        prevMap = {}
-        
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff], i]
-            prevMap[n] = i
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            check = target - nums[i]
+            if check in nums:
+                if i!=nums.index(check):
+                    return list([i,nums.index(check)])
